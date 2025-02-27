4eeae92107a68d83e8c47cc0595069b33787143f
LDragon <ldragon3919@gmail.com>
2025-02-27 21:30:39 +0700

    k

---
+++ src/test/java/com/example/lt272/Lt272ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lt272;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lt272ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
